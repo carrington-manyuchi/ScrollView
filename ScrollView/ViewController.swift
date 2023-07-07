62e7c9217a1ba1091943ceb8a410985b2024aa1d
Carrington <Manyuchi>
2023-07-07 20:52:47 +0200

    scrollView

--- ScrollView/ViewController.swift
+++ ScrollView/ViewController.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let myScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.contentSize = CGSize(width: 600, height: 300  )
+        scrollView.contentOffset = CGPoint(x: 0, y: 0)
+        return scrollView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
