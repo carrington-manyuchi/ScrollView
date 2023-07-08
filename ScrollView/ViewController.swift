dfed3f172707bbdb1fb6a086556c627c4f28c7e3
Carrington <Manyuchi>
2023-07-08 13:58:19 +0200

    added

--- ScrollView/ViewController.swift
+++ ScrollView/ViewController.swift
@@ -8,20 +8,62 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var coolScrollView: UIScrollView!
     
     private let myScrollView: UIScrollView = {
-        let scrollView = UIScrollView()
+        
+        
+        let scrollView = UIScrollView(frame: CGRect(x: 0, y: 60, width: 900, height: 300))
+                                      
         scrollView.translatesAutoresizingMaskIntoConstraints = false
-        scrollView.contentSize = CGSize(width: 600, height: 300  )
-        scrollView.contentOffset = CGPoint(x: 0, y: 0)
+        //scrollView.contentOffset = CGPoint(x: 0, y: 0)
+        
+      
+        //scrollView.contentSize.width = CGFloat(900)
+        //scrollView.contentSize.height =  CGFloat(300)
+       // scrollView.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        scrollView.isScrollEnabled = true
+        scrollView.backgroundColor = UIColor.red
+        scrollView.isDirectionalLockEnabled = false
+        scrollView.bounces = true
+        scrollView.isPagingEnabled = true
+        scrollView.isScrollEnabled = true
+        scrollView.showsHorizontalScrollIndicator = true
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.scrollIndicatorInsets = UIEdgeInsets.zero
+        scrollView.indicatorStyle = .black
+        scrollView.minimumZoomScale = 1
+        scrollView.maximumZoomScale = 10
+        scrollView.flashScrollIndicators()
+        scrollView.bouncesZoom = true
+        scrollView.scrollsToTop = true
+        scrollView.clipsToBounds = false
+        
         return scrollView
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(myScrollView)
+        print(myScrollView)
     }
-
+    
+    
+    private func configureConstraints() {
+        
+        let myScrollViewConstraints = [
+            myScrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            myScrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            myScrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            myScrollView.widthAnchor.constraint(equalToConstant: 900),
+            myScrollView.bottomAnchor.constraint(equalTo: coolScrollView.topAnchor)
+        ]
+        
+        
+        NSLayoutConstraint.activate(myScrollViewConstraints)
+    }
+    
+    
 
 }
 
